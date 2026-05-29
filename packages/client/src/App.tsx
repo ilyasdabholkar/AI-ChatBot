b75ec3f4f5e049bdf0208fd6ff089c4c249b49ac
ilyas dabholkar <ilyasdabholkar9@gmail.com>
2026-05-29 20:12:45 +0530

    Reafactoring and integrating APIs into frontend

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,20 +1,10 @@
-import { useEffect, useState } from 'react';
-import { Button } from './components/ui/button';
+import ChatBot from './components/chat/ChatBot';
 
 function App() {
-   const [message, setMessage] = useState('');
-
-   useEffect(() => {
-      fetch('/api/hello')
-         .then((res) => res.json())
-         .then((data) => setMessage(data.message));
-   });
-
    return (
-      <>
-         <p className="font-bold p-4 text-3xl">{message}</p>
-         <Button variant="outline">Button</Button>
-      </>
+      <div className="p-4 h-screen">
+         <ChatBot />
+      </div>
    );
 }
 
